67055a9553bc321585980e2946f886bef523ab5c
Oleh Krupenia <oleh.krupenia@globallogic.com>
2016-08-03 15:46:13 +0300

    bean life cycle

--- src/main/java/com/springinaction/knights/BraveKnight.java
+++ src/main/java/com/springinaction/knights/BraveKnight.java
@@ -1,9 +1,16 @@
 package com.springinaction.knights;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * @author oleh.krupenia.
  */
-public class BraveKnight implements Knight {
+public class BraveKnight implements Knight, BeanNameAware, BeanFactoryAware, ApplicationContextAware, BeanPostProcessor,
+        DisposableBean, InitializingBean {
     private Quest quest;
 
     public BraveKnight(Quest quest) {
@@ -12,5 +19,50 @@ public class BraveKnight implements Knight {
 
     public void embarkOnQuest() throws QuestException {
         quest.embark();
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("setBeanName");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("setApplicationContext");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessBeforeInitialization");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("postProcessAfterInitialization");
+        return bean;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+    }
+
+    public void before() {
+        System.out.println("custom before");
+    }
+
+    public void after() {
+        System.out.println("custom after");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
     }
 }
